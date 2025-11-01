f2e8066d8eb8a7b556775bd4afbc0addf3728909
Haseeb Ur Rehman <167428215+Haseeb55859@users.noreply.github.com>
2025-11-01 14:50:03 +0500

    Mid Projeect

---
+++ detail.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.all(10),
+      children: [
+        Text(
+          'DESTINATION',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(height: 10),
+        Center(
+          child: Image.asset(
+            'assets/images/img8.jpeg',
+            height: 250,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(height: 15),
+        Text(
+          'Bali, Indonesia',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 5),
+        Row(
+          children: [
+            Icon(Icons.star, color: Colors.orange, size: 20),
+            SizedBox(width: 5),
+            Text(
+              '4.8   43 Reviews',
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          ],
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Beautiful beaches, temples, and sunsets.',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+        SizedBox(height: 15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.wifi, color: Colors.white),
+            Icon(Icons.fastfood, color: Colors.white),
+            Icon(Icons.directions_bus, color: Colors.white),
+          ],
+        ),
+        SizedBox(height: 25),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor:  Color.fromARGB(255, 91, 54, 97),
+            padding: EdgeInsets.symmetric(vertical: 15),
+          ),
+          child: Text(
+            'Book Now',
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- main.dart
+++ main.dart
@@ -1,37 +1,157 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/menu.dart';
+import 'detail.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Widget Tree")),
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: <Widget>[
-                  // LoginForm(), 
-                  // Row 1
-                  Row1(),
-                  const Padding(padding: EdgeInsets.all(16.0)),
-                  // Row 2
-                  Row2(),
-                ],
-              ),
-            ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.deepPurple,
+          titleTextStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.white,
           ),
+          centerTitle: true,
         ),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    HomeScreen(),
+    DetailsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_selectedIndex == 0 ? 'TRAVEL APP' : 'DETAILS PAGE'),
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.deepPurpleAccent,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.travel_explore), label: "Details"),
+        ],
       ),
     );
   }
 }
 
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List destinations = [
+      {'name': 'Paris', 'url': 'assets/images/img2.jpg'},
+      {'name': 'New York', 'url': 'assets/images/img3.jpg'},
+      {'name': 'Bali', 'url': 'assets/images/img4.jpeg'},
+      {'name': 'Dubai', 'url': 'assets/images/img5.jpeg'},
+      {'name': 'Sydney', 'url': 'assets/images/img6.jpeg'},
+      {'name': 'Riyadh', 'url': 'assets/images/img7.jpeg'},
+    ];
+
+    return ListView(
+      padding: EdgeInsets.all(10),
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Explore',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+            ),
+            Icon(Icons.person_outline, size: 30),
+          ],
+        ),
+        SizedBox(height: 10),
+        Center(
+          child: Image.asset(
+            'assets/images/img1.jpeg',
+            height: 250,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(height: 15),
+        TextField(
+          style: TextStyle(color: Colors.white), 
+          decoration: InputDecoration(
+            hintText: 'Where to?',
+            hintStyle: TextStyle(color: Colors.white70),
+            border: OutlineInputBorder(),
+            prefixIcon: Icon(Icons.search, color: Colors.white70),
+          ),
+        ),
+        SizedBox(height: 15),
+        Text(
+          'POPULAR DESTINATIONS',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+        ),
+        SizedBox(height: 10),
+        SizedBox(
+          height: 600,
+          child: ListView.builder(
+            itemCount: destinations.length,
+            itemBuilder: (context, index) {
+              final destination = destinations[index];
+              return Padding(
+                padding: EdgeInsets.only(bottom: 12.0),
+                child: ListTile(
+                  tileColor: Colors.grey[900],
+                  leading: Image.asset(
+                    destination['url'],
+                    width: 90,
+                    height: 90,
+                    fit: BoxFit.cover,
+                  ),
+                  title: Text(
+                    destination['name'],
+                    style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('Discover More'),
+        ),
+      ],
+    );
+  }
+}
